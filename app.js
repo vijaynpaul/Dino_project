e32983ced1d0ca1d1fc324228efe1e8dbc6277d9
vijaynpaul <61095605+vijaynpaul@users.noreply.github.com>
2022-04-29 12:00:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// Create Dino Constructor
+
+
+
+// Create Dino Objects
+
+
+// Create Human Object
+
+// Use IIFE to get human data from form
+
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches. 
+
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
+
+fetch("/dino.json").then(response => response.json()).then(data => console.log(data))
